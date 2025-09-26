aed9809da5bfa7c1d5327c867d6b5f86e041572d
Tino Hager <tino.hager@nager.at>
2025-09-26 16:21:07 +0200

    Update app-header.vue

--- src/app/components/app-header.vue
+++ src/app/components/app-header.vue
@@ -29,5 +29,24 @@ const items = ref<NavigationMenuItem[][]>([
 <template>
   <UHeader title="Nuxt UI">
     <UNavigationMenu color="neutral" :items="items" class="w-full" />
+
+        <template #right>
+      <UColorModeButton />
+
+      <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
+        <UButton
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/nuxt/ui"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+        />
+      </UTooltip>
+    </template>
+
+    <template #body>
+      <UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5" />
+    </template>
   </UHeader>
 </template>
